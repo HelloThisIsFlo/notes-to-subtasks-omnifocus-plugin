9059c12f204843e667313e68bb9c1450bbfbeaaa
Flo Kempenich <flo@kempenich.dev>
2023-04-20 18:29:18 +0100

    Update install location

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 
-cp -r ./*.omnifocusjs ~/Library/Mobile\ Documents/iCloud~com~omnigroup~OmniFocus/Documents/Plug-Ins/
+cp -r ./*.omnifocusjs "$HOME/Library/Mobile Documents/iCloud~com~omnigroup~OmniFocus/Documents/Plug-Ins/Other/Keyboard Shortcuts"
 
 
 cd - >/dev/null
